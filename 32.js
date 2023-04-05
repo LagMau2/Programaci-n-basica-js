ea29d5b2ba36cfadc60af9a5099fb1a03c446052
Mau Ramirez <a348705@uach.mx>
2023-04-04 18:09:58 -0600

    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,5 @@
+const personas = ['Juan', 'María', 'Pedro', 'Ana', 'Luis', 'Carla', 'José', 'Marta', 'Pablo', 'Sofía', 'Diego', 'Laura', 'Javier', 'Lucía', 'Carlos', 'Isabel', 'Fernando', 'Lucas', 'Luciana', 'Elena'];
+
+for (let i = 0; i < personas.length; i++) {
+  console.log(personas[i]);
+}
